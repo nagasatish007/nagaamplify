0ec6379567befdf2701774cd8f8aa4dc72fefbda
Naga <satish.1290@gmail.com>
2021-04-30 15:43:48 +0530

    Second Commit

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
 		        <header className="App-header">
 		          <img src={logo} className="App-logo" alt="logo" />
 		          <h1>Hello from NagaSatish</h1>
+				  <h2>How Are You Today?</h2>
 		        </header>
 		      </div>
 		    );
